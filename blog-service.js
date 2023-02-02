48e776f362342565bd72410a6035e83879bdd455
Bhawanjot <bhawanjot20c@gmail.com>
2023-02-02 13:59:48 -0500

    update

--- blog-service.js
+++ blog-service.js
@@ -5,6 +5,16 @@ const app = express();
 let posts = [];
 let categories = [];
 
+fs.readFile('./data/posts.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+  posts = JSON.parse(data);
+});
+
+fs.readFile('./data/categories.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+  categories = JSON.parse(data);
+});
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -20,11 +30,11 @@ app.get('/blog', (req, res) => {
   res.json(publishedPosts);
 });
 
-app.get('./data/posts.json', (req, res) => {
+app.get('/data/posts.json', (req, res) => {
   res.json(posts);
 });
 
-app.get('/categories.json', (req, res) => {
+app.get('/data/categories.json', (req, res) => {
   res.json(categories);
 });
 
--- server.js
+++ server.js
@@ -26,6 +26,25 @@ app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
+// Route "/blog" returns the blog.html file from the views folder
+app.get('/blog', (req, res) => {
+  res.sendFile(path.join(__dirname,'blog-service.js'));
+});
+
+// Route "/categories" returns the categories.html file from the views folder
+app.get('/categories', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data','categories.json'));
+});
+
+// Route "/posts" returns the posts.html file from the views folder
+app.get('/posts', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data','posts.json'));
+});
+
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
+
 // Start the server on process.env.PORT || 8080
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
