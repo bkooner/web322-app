48aa8c23ae1804f8570a24819a14020aee9186a7
Bhawanjot <bhawanjot20c@gmail.com>
2023-02-03 17:13:48 -0500

    update changes

--- blog-service.js
+++ blog-service.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Bhawanjot Singh Kooner Student ID: 167834217 Date: 2023-02-02
+*
+*  Online (Cyclic) Link: https://cautious-buckle-colt.cyclic.app
+*
+********************************************************************************/ 
 const fs = require('fs');
 const path = require("path");
 let posts = [];
--- server.js
+++ server.js
@@ -12,7 +12,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const { initialize, getAllPosts,getPublishedPosts, getCategories} = require('./blog-service.js');
+const blog = require('./blog-service.js');
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 const HTTP_PORT = process.env.PORT || 8080;
@@ -29,30 +29,31 @@ app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "about.html"));
 });
 
-app.get("/blog", (req, res) => {
-  getPublishedPosts()
-    .then(data => res.send(data))
-    .catch(err => res.status(500).send(err.message));
+blog.initialize().then(() => {
+  app.listen(HTTP_PORT, onHttpStart());
+})
+.catch (() => {
+    console.log('Error');
 });
 
 app.get("/posts", (req, res) => {
-  getAllPosts()
+  blog.getAllPosts()
     .then(data => res.send(data))
-    .catch(err => res.status(500).send(err.message));
+    .catch(err => res.send(err.message));
+});
+
+app.get("/blog", (req, res) => {
+  blog.getPublishedPosts()
+    .then(data => res.send(data))
+    .catch(err => res.send(err.message));
 });
 
 app.get("/categories", (req, res) => {
-  getCategories()
+  blog.getCategories()
     .then(data => res.send(data))
-    .catch(err => res.status(500).send(err.message));
+    .catch(err => res.send(err.message));
 });
 
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "pageNotFound.html"));
-});
-
-initialize().then(() => {
-  app.listen(HTTP_PORT, onHttpStart());
-}).catch (() => {
-    console.log('Error');
 });
